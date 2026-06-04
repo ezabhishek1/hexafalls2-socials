18921efdcd9cf57f0b255dae5780b4af344d7104
Abhishek Gupta <ezabhishek1@users.noreply.github.com>
2026-06-05 01:36:51 +0530

    refactor: prune unused files, components, styles, and assets

--- components/SplineModel.tsx
+++
@@ -1,9 +0,0 @@
-import Spline from "@splinetool/react-spline/next";
-
-export default function SplineModel() {
-  return (
-    <main>
-      <Spline scene="https://prod.spline.design/rzxy-WQFTuMYe1q2/scene.splinecode" />
-    </main>
-  );
-}
